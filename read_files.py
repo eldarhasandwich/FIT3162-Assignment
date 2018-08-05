0d989f701dd302b7cb969920febcf229770ae4ba
ajbri8 <34791836+ajbri8@users.noreply.github.com>
2018-08-06 00:50:26 +1000

    added nodes containing edges

--- read_files.py
+++ read_files.py
@@ -6,6 +6,8 @@ class AdjacencyMatrix:
         self.vals = {}
         self.c = 0
         self.elements = [[0 for _ in range(10000)] for _ in range(10000)]
+        self.nodes = []
+        self.output_file = "test.txt"
 
 
     def add(self, sender, receiver):
@@ -26,22 +28,44 @@ class AdjacencyMatrix:
         except IndexError:
             print(row, col)
 
+    def write_data_to_textfile(self):
+        output = open(self.output_file, 'w')
+        N = self.c
+        for i in range(N):
+            edges = []
+            has_sent = False
+            sender = self.vals[i]
+            for j in range(N):
+                if i != j:
+                    recipient = self.vals[j]
+                    val = self.elements[i][j]
+                    if val > 0:
+                        edges.append((recipient, val))
+                        has_sent = True
+                        output.write(str(val) + ": " + recipient)
+                        output.write("\n")
+            if has_sent:
+                output.write("WERE SENT FROM: " + sender)
+                output.write("\n")
+                output.write("\n")
+            sender_node = Node(sender, edges)
+            self.nodes.append(sender_node)
+
+
+
 class FileProcessor:
     def __init__(self):
         self.directory = "C:\\Users\\Valued Customer\\Desktop\\maildir"
         self.email_reader = FileReader()
         self.adj_matrix = AdjacencyMatrix()
-        self.output_file = "test.txt"
-
-
 
     def process_all_files_in_directory(self):
         file_count = 0
         file_limit = 5000
         for dir_name, subdir, file_list in os.walk(self.directory):
+            if file_count > file_limit:
+                break
             if "sent" in dir_name:
-                #if file_count > file_limit:
-                    #return
                 for file_name in file_list:
                     file_count += 1
                     file_location = str(dir_name + "\\" + file_name)
@@ -49,42 +73,7 @@ class FileProcessor:
                     if sender and receiver:
                         self.adj_matrix.add(sender, receiver)
 
-
-
-    def write_emails_to_textfile(self):
-        output = open(self.output_file, 'w')
-        line = 0
-        max_count = 0
-        email_count = 0
-        N = self.adj_matrix.c
-        for i in range(N):
-            has_sent = False
-            sender = self.adj_matrix.vals[i]
-            for j in range(N):
-                if i != j:
-                    recipient = self.adj_matrix.vals[j]
-                    val = self.adj_matrix.elements[i][j]
-                    if val > 0:
-                        has_sent = True
-                        if val > max_count:
-                            max_count = val
-                        email_count += val
-                        output.write(str(val) + " emails to " + recipient)
-                        output.write("\n")
-                        line += 1
-            if has_sent:
-                output.write("WERE SENT FROM: " + sender)
-                output.write("\n")
-                output.write("\n")
-
-        output.write("There are {0} unique email addresses ending with @enron.com".format(str(N)))
-        output.write("\n")
-        output.write("In total, {0} emails were sent amongst these addresses".format(str(email_count)))
-        output.write("\n")
-        output.write("The highest number of emails sent from one address to another was {0}".format(str(max_count)))
-
-
-
+        return self.adj_matrix
 
 
 class FileReader:
@@ -92,8 +81,6 @@ class FileReader:
         self.sender_substring = "From:"
         self.receiver_substring = "To:"
         self.email_suffix = "@enron.com"
-
-
 
     def get_sender_and_receiver(self, email):
         sender, receiver = None, None
@@ -109,8 +96,6 @@ class FileReader:
                     break
         return sender, receiver
 
-
-
     def get_email_address(self, a_string):
         if a_string.endswith(self.email_suffix):
             email_str = ""
@@ -119,23 +104,48 @@ class FileReader:
                 char = a_string[i]
                 email_str += char
                 i -= 1
-
             email_str.join(email_str.split())
             start_i = len(self.email_suffix)
             if self.email_is_valid(email_str[start_i:]):
                 return email_str[::-1]
 
     def email_is_valid(self, a_string):
+        a_bool = False
         m = re.search(r"^[a-z]+([.])[a-z]+$", a_string)
         if m:
-            return m.group() == a_string
-        else:
-            return False
+            a_bool = (m.group() == a_string)
+        return a_bool
 
 
-email_finder = FileProcessor()
-email_finder.process_all_files_in_directory()
-email_finder.write_emails_to_textfile()
+class Node:
+    def __init__(self, email, edges):
+        self.email = email
+        self.edges = edges
+
+    def toString(self):
+        return str(self.email)
+
+    def edges_as_strings(self):
+        for edge in self.edges:
+            print(edge)
+
+
+
+
+
+file_processor = FileProcessor()
+adj_matrix = file_processor.process_all_files_in_directory()
+adj_matrix.write_data_to_textfile()
+for node in adj_matrix.nodes:
+    print(node.toString())
+    node.edges_as_strings()
+    break
+
+
+
+
+
+
 
 
 
