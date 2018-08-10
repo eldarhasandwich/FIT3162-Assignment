e394d85b75a7770519b7fa60a34f6d20c92372de
eldarhasandwich <hasanovicel@gmail.com>
2018-08-10 12:40:23 +1000

    add some db helper methods

--- databaseController.py
+++ databaseController.py
@@ -5,6 +5,11 @@ from classes.AdjacencyList import *
 
 conn = psycopg2.connect("dbname=testsnagraphs user=eldar")
 
+# CREATE
+# READ
+# UPDATE
+# DELETE
+
 """
 Some notes about psycopg, its trash.
 Details: There does not seem to be safe built in error catching for very simple exception scenarios,
@@ -21,7 +26,7 @@ def RunQuery (qString):
 # create new graph
 # params: graph name
 # returns: new graph ID
-def CreateNewGraph (newName):
+def CREATE_NewGraph (newName):
     cur = conn.cursor()
     cur.execute("INSERT INTO graphs (name) VALUES ('" + newName + "') RETURNING id;")
     newID = cur.fetchall()[0]
@@ -32,7 +37,7 @@ def CreateNewGraph (newName):
 # create new node
 # params: graphID, data (node emailAddr)
 # returns: new nodeID
-def CreateNewNode (graphID, data):
+def CREATE_NewNode (graphID, data):
     cur = conn.cursor()
     cur.execute("INSERT INTO nodes (email, graph_ID) VALUES (%s, %s) RETURNING id;", (data, graphID))
     newID = cur.fetchall()[0]
@@ -42,57 +47,52 @@ def CreateNewNode (graphID, data):
 
 # create new edge
 # params: graphID, senderID, recipID, data (emailcount)
-# returns: TODO: return edgeID
-def CreateNewEdge (graphID, senderID, recipientID, data):
+# returns: new edgeID
+def CREATE_NewEdge (graphID, senderID, recipientID, data):
     cur = conn.cursor()
-    cur.execute("INSERT INTO edges (graph_ID, sender_ID, recipient_ID, email_count) VALUES (%s, %s, %s, %s) RETURNING id", (graphID, senderID, recipientID, data))
+    cur.execute(
+        "INSERT INTO edges (graph_ID, sender_ID, recipient_ID, email_count) VALUES (%s, %s, %s, %s) RETURNING id", 
+        (graphID, senderID, recipientID, data)
+    )
     newID = cur.fetchall()[0]
     conn.commit()
     cur.close()
     return newID
 
-def UpdateEdgeData (edgeID, data):
+# returns array of all graphObjects
+def READ_AllGraphs ():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM graphs")
+    value = cur.fetchall()
+    cur.close()
+    return value
+
+def READ_NodesInGraph (graphID):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM nodes WHERE graph_ID = %s", (graphID))
+    value = cur.fetchall()
+    cur.close()
+    return value
+
+def READ_EdgesInGraph (graphID):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM edges WHERE graph_ID = %s", (graphID))
+    value = cur.fetchall()
+    cur.close()
+    return value
+
+# update edge data
+# params: edgeID, data (emailcount)
+def UPDATE_EdgeData (edgeID, data):
     cur = conn.cursor()
     cur.execute("UPDATE edges SET email_count = %s WHERE id = %s", (data, edgeID))
     conn.commit()
-    cur.close()
-
-# def PullMatrixFromDB (graphID):
-#     graphID = str(graphID) #Typeerror without this line
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM nodes WHERE graph_ID = %s", (graphID))
-#     nodes = cur.fetchall()
-#     cur.execute("SELECT * FROM edges WHERE graph_ID = %s", (graphID))
-#     edges = cur.fetchall()
-#     cur.close()
-
-#     print("Nodes:")
-#     for n in nodes: print(n)
-#     print("Edges:")
-#     for e in edges: print(e)
-
-#     matrix = AdjacencyMatrix()
-#     for e in edges: # this is why python is an abomination
-#         sender = filter(lambda x : x[0] == e[1], nodes)[0][1]
-#         receiver = filter(lambda x : x[0] == e[2], nodes)[0][1]
-
-#         matrix.add(sender, receiver)
-#         print(sender, receiver)
-#     matrix.write_data_to_textfile()        
+    cur.close()   
 
 def PullListFromDB (graphID):
     graphID = str(graphID) #Typeerror without this line
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM nodes WHERE graph_ID = %s", (graphID))
-    nodes = cur.fetchall()
-    cur.execute("SELECT * FROM edges WHERE graph_ID = %s", (graphID))
-    edges = cur.fetchall()
-    cur.close()
-
-    # print("Nodes:")
-    # for n in nodes: print(n)
-    # print("Edges:")
-    # for e in edges: print(e)
+    nodes = READ_NodesInGraph(graphID)
+    edges = READ_EdgesInGraph(graphID)
 
     adjList = AdjacencyList()
     for e in edges:
@@ -117,8 +117,6 @@ def PushListToDB (graphID, _AdjacencyList):
             nodeDict[key] = {"key": key, "address": sender.emailAddress, "graphID": graphID}
         else: pass
         for key, recipient in sender.recipients.items():
-            # print("r_key", key)
-            # print("r_obj", recipient.emailAddress)
             if key not in nodeDict:
                 nodeDict[key] = {"key": key, "address": sender.emailAddress, "graphID": graphID}
             else: pass
@@ -138,4 +136,5 @@ def PushListToDB (graphID, _AdjacencyList):
 adjList = PullListFromDB(1)
 PushListToDB(1, adjList)
 
-print(CreateNewGraph("heyA"))
+# print(CreateNewGraph("heyA"))
+print(READ_AllGraphs())
