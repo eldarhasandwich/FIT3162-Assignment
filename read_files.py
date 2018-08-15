9fcd2a836d9e306153380701f8c14897cd8cce9c
ajbri8 <34791836+ajbri8@users.noreply.github.com>
2018-08-16 00:23:22 +1000

    added working group class
    added 'group' objects for any email sent to two or more receivers, with individual adjacency list to determine specific flows of communication within a single group

--- read_files.py
+++ read_files.py
@@ -1,100 +1,243 @@
 import os, re, string
 
+
 class FileProcessor:
-    def __init__(self):
-        self.directory = "C:\\Users\\Valued Customer\\Desktop\\maildir"
-        self.email_reader = FileReader()
+    def __init__(self, directory):
+        self.directory = directory
+        self.email_reader = EmailReader()
+        self.email_container = EmailContainer()
+        self.groups = Groups()
 
 
-    def process_all_files_in_directory(self):
+    def process_directory_files(self):
+        file_type = "sent"
+        my_emails = []
         file_count = 0
         file_limit = 5000
         for dir_name, subdir, file_list in os.walk(self.directory):
-            #if file_count > file_limit:
-                #break
-            if "sent" in dir_name:
-                for file_name in file_list:
-                    file_count += 1
-                    file_location = str(dir_name + "\\" + file_name)
-                    sender, receivers = self.email_reader.get_sender_and_receiver(file_location)
+            if file_count < file_limit:
+                if file_type in dir_name:
+                    for file_name in file_list:
+                        file_count += 1
+                        file_location = self.concatenate_file_location(dir_name, file_name)
+                        sender, receivers, id, group_members = self.email_reader.get_email_attributes(file_location)
+                        if sender and receivers and id:
+                            if self.email_container.email_is_unique(sender, receivers, id):
+                                self.email_container.add_email(sender, receivers, id)
+                        if group_members:
+                            self.groups.add(group_members)
 
-class FileReader:
+        return my_emails
+
+    def concatenate_file_location(self, dir_name, file_name):
+        return str(dir_name + "\\" + file_name)
+
+class EmailReader:
     def __init__(self):
-        self.sender_substring = "From:"
-        self.receiver_substring = "To:"
-        self.email_suffix = "@enron.com"
-        self.output = open("output.txt", 'w')
+        self.temp = "Hi"
 
-    def get_sender_and_receiver(self, email):
-        sender, receivers = None, None
+
+    def get_email_attributes(self, email):
+        group_members = None
+        sender, receivers, id = None, None, None
         with open(email, 'r') as f:
             contents = f.readlines()
             for line in contents:
                 line = line.rstrip('\n')
-                if self.sender_substring in line and not sender:
-                    sender = self.get_email_address(line)
-                elif line.startswith(self.receiver_substring) and not receivers:
-                    receivers = self.get_all_receivers(line)
-                if sender and receivers:
-                    self.output.write("Email sent by "+str(sender)+" to "+', '.join(receivers))
-                    self.output.write("\n")
+                if not id:
+                    id = self.get_id(line)
+                if not sender:
+                    sender = self.get_sender(line)
+                if not receivers:
+                    receivers = self.get_receivers(line)
+                if sender and receivers and id:
+                    if len(receivers) > 1:
+                        group_members = [sender]
+                        for i in receivers:
+                            group_members.append(i)
+
                     break
-        return sender, receivers
-
-    def get_all_receivers(self, a_string):
-        stored_receivers = []
-        a_string = a_string.replace(" ", "")
-        a_string = a_string[3:]
-        all_receivers = a_string.split(",")
-        for receiver in all_receivers:
-            valid_receiver = receiver.translate(string.punctuation)
-            val = self.get_email_address(valid_receiver)
-            if val:
-                if self.email_is_valid(val):
-                    stored_receivers.append(val)
-        if len(stored_receivers) > 0:
-            return stored_receivers
 
 
+        return sender, receivers, id, group_members
 
+    def get_receivers(self, a_string):
+        receiver_substring = "To:"
+        if a_string.startswith(receiver_substring):
+            receivers = []
+            a_string = a_string.replace(" ", "")[3:]
+            all_receivers = a_string.split(",")
+            for receiver in all_receivers:
+                cleaned_receiver = self.remove_punctuation(receiver)
+                receiver_name = self.get_email_address(cleaned_receiver)
+                if self.address_is_valid(receiver_name):
+                    receivers.append(receiver_name)
+
+            if len(receivers) > 0:
+                return receivers
+
+    def address_is_valid(self, a_string):
+        return self.email_structure_is_correct(a_string)
+
+    def remove_punctuation(self, a_string):
+        return a_string.translate(string.punctuation)
+
+    def get_id(self, a_string):
+        id_substring = "Message-ID"
+        if id_substring in a_string:
+            return "".join([i for i in a_string if i.isdigit()])
+
+    def get_sender(self, a_string):
+        sender_substring = "From:"
+        if sender_substring in a_string:
+            return self.get_email_address(a_string)
 
     def get_email_address(self, a_string):
-        if a_string.endswith(self.email_suffix):
-            email_str = ""
+        email_suffix = "@enron.com"
+        email_str = ""
+        if a_string.endswith(email_suffix):
             i = len(a_string) - 1
             while a_string[i] != " " and i >= 0:
                 char = a_string[i]
                 email_str += char
                 i -= 1
             email_str.join(email_str.split())
-            start_i = len(self.email_suffix)
-            email_str = email_str[start_i:][::-1]
-            return email_str
+            j = len(email_suffix)
+            email_str = email_str[j:][::-1]
+        return email_str
 
-    def email_is_valid(self, a_string):
+    def email_structure_is_correct(self, a_string):
         m = re.search(r"^[a-z]+([.])[a-z]+$", a_string)
         if m:
             return True
         return False
 
 
-#create email object with email text file - get below attributes in class
-#store all emails as objects in a emailDict class to look for duplicates
 class Email:
-    def __init__(self, sender, receiver, id, date, time):
+    def __init__(self, sender, receivers, message_id):
         self.sender = sender
-        self.receiver = receiver
-        self.id = id
-        self.date = date
-        self.time = time
+        self.receivers = receivers
+        self.message_id = message_id
 
-file_processor = FileProcessor()
-file_processor.process_all_files_in_directory()
+    @staticmethod
+    def static_identifier_string(sender, receivers, message_id):
+        return str(sender + " " + ', '.join(receivers) + ' {0}'.format(message_id))
+
+    def identifier_string(self):
+        return str(self.sender + " " + ', '.join(self.receivers) + ' {0}'.format(self.message_id))
+
+class EmailContainer:
+    def __init__(self):
+        self.email_identifiers = {}
+        self.unique_emails = []
+
+    def email_is_unique(self, sender, receivers, id):
+        return Email.static_identifier_string(sender, receivers, id) not in self.email_identifiers
+
+    def add_email(self, sender, receivers, id):
+        self.email_identifiers[Email.static_identifier_string(sender, receivers, id)] = True
+        self.unique_emails.append(Email(sender, receivers, id))
+
+    def get_emails(self):
+        return self.unique_emails
+
+    def add_to_adjacency_list(self, adj_list):
+        return
+
+class Group:
+    def __init__(self, sorted_members, sender, receivers, key):
+        self.sorted_members = sorted_members
+        self.original_sender = sender
+        self.original_receivers = receivers
+        self.key = key
+        self.id_dict = self.cons_id_dict()
+        self.adj_list = self.cons_adj_list()
+
+
+    def cons_id_dict(self):
+        c = 0
+        id_dict = {}
+        for i in self.sorted_members:
+            id_dict[i] = c
+            c += 1
+        return id_dict
+
+    def cons_adj_list(self):
+        adj_list = {}
+        for i in self.sorted_members:
+            adj_list[i] = []
+            for j in self.sorted_members:
+                adj_list[i].append((j, 0))
+
+        for key in self.sorted_members:
+            index = self.id_dict[key]
+            if key != self.original_sender:
+                c = 1
+            else:
+                c = 0
+            val = (key, c)
+            adj_list[self.original_sender][index] = val
+
+        for key in self.sorted_members:
+            if key != self.original_sender:
+                for j in range(len(self.sorted_members)):
+                    index = self.id_dict[self.sorted_members[j]]
+                    val = (self.sorted_members[j], 0)
+                    adj_list[key][index] = val
+
+            return adj_list
+
+    def add_email(self, sender, receivers):
+        for receiver in receivers:
+            index = self.id_dict[receiver]
+            values = self.adj_list[sender]
+            val = values[index]
+            new_val = (val[0], val[1] + 1)
+            self.adj_list[sender][index] = new_val
+
+    def write_to_file(self, output):
+        output.write("Emails sent exclusively between the group of" + self.key)
+        output.write("\n")
+        for vertex in self.adj_list:
+            for edge in self.adj_list[vertex]:
+                edge_string = edge[0]
+                edge_val = edge[1]
+                output.write(vertex +" sent " + str(edge_val) + " to " + str(edge_string))
+                output.write("\n")
+
+
+class Groups:
+    def __init__(self):
+        self.elements = {}
+
+
+    def add(self, members):
+        assert isinstance(members, list)
+        assert len(members) > 2
+        sender = members[0]
+        receivers = members[1:]
+        members.sort()
+        key = self.create_dict_key(members)
+        if key not in self.elements:
+            self.elements[key] = Group(members, sender, receivers, key)
+        else:
+            self.elements[key].add_email(sender, receivers)
+
+    def create_dict_key(self, members):
+        my_str = " "
+        for e in members:
+            my_str += str(e) + ", "
+        return my_str[:-2]
 
 
 
 
 
+my_directory = "C:\\Users\\Valued Customer\\Desktop\\maildir"
+group_output = open("output.txt", 'w')
+file_processor = FileProcessor(my_directory)
+all_emails = file_processor.process_directory_files()
+all_groups = file_processor.groups.elements
 
-
-
+for group in all_groups:
+    all_groups[group].write_to_file(group_output)
