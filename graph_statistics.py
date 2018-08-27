4afa979545be7fa3eebaadf5ced348b3a0425488
ajbri8 <34791836+ajbri8@users.noreply.github.com>
2018-08-27 12:16:48 +1000

    added BFS to find shortest path between nodes

--- graph_statistics.py
+++ graph_statistics.py
@@ -1,4 +1,5 @@
 import classes.AdjacencyList as AL
+from collections import deque
 
 class GraphStatistics:
     def __init__(self):
@@ -39,10 +40,46 @@ class GraphStatistics:
     def get_density(self):
         edges = self.number_of_edges()
         max_number_of_edges = self.max_edges()
-        return edges / max_number_of_edges
+        return float(edges / max_number_of_edges)
+
+    #No need to use Dijkstra's here because graph is unweighted, instead use BFS
+    def shortest_path_between_two_nodes(self, start, end):
+        path_found = False
+        visited = {i: False for i in self.nodes}
+        previous = {j: None for j in self.nodes}
+        prev_node = None
+        my_queue = deque()
+        my_queue.append(start)
+
+        while my_queue:
+            vertex = my_queue.pop()
+            if visited[vertex] is False:
+                visited[vertex] = True
+                if prev_node:
+                    previous[vertex] = prev_node
+                if vertex in self.adj_list:
+                    edges = self.adj_list[vertex]
+                    for node in edges:
+                        node = node[0]
+                        my_queue.append(node)
+                        if node == end:
+                            path_found = True
+                            if not prev_node:
+                                prev_node = vertex
+                            previous[node] = prev_node
+                            break
+
+                prev_node = vertex
+                if path_found:
+                    break
 
 
-
-
-
-
+        if not path_found:
+            return "No path"
+        else:
+            c = 0
+            next_node = end
+            while previous[next_node]:
+                c += 1
+                next_node = previous[next_node]
+        return c
