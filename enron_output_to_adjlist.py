b8dd12bd14bcdbeb0eb984923783dfb63cf5b5a6
ajbri8 <34791836+ajbri8@users.noreply.github.com>
2018-08-27 11:22:01 +1000

    added computation of density

--- enron_output_to_adjlist.py
+++ enron_output_to_adjlist.py
@@ -1,5 +1,5 @@
-
 import classes.AdjacencyList as AL
+from graph_statistics import GraphStatistics
 
 def LineToIndividuals(line):
     if line == ['']: return False, False
@@ -14,37 +14,11 @@ def LineToIndividuals(line):
     recipients = relaventWords[1:]
     return sender, recipients
 
-# returns message, isValid
-def EnronOutputIsValid(file):
-    text = file.read()
-    file.close()
-
-    lines = text.split("\n")
-    for line in lines:
-        words = line.split(" ")
-        if words[0] != "Email" or words[1] != "sent" or words[2] != "by" or words[4] != "to":
-            return "Error: " + line + " is an invalid line", False
-    return "This file can be parsed", True
 
 def EnronOutputToAdjList():
-    f = open("plaintexts/enron_output.txt", 'r')
+    f = open("enron_output.txt", 'r')
     text = f.read()
     f.close()
-
-    lines = text.split("\n")
-
-    adjList = AL.AdjacencyList()
-    for l in lines:
-        s, r = LineToIndividuals(l)
-
-        for recip in r:
-            adjList.AddSenderRecipientPair(s, s, recip, recip)
-
-    return adjList
-
-def TxtToAdjList(file):
-    text = file.read()
-    file.close()
 
     lines = text.split("\n")
 
@@ -60,4 +34,11 @@ def TxtToAdjList(file):
 
 
 if __name__ == "__main__":
-    print(EnronOutputToAdjList())
+    adj_list = EnronOutputToAdjList()
+    graph_statistics = GraphStatistics()
+    for sender in adj_list.senders:
+        sender = adj_list.get_sender_as_object(sender)
+        graph_statistics.add_data(sender)
+        break
+
+    print(graph_statistics.get_density())
